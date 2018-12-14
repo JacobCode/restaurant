a88e39f852c2790f3cd6bbe05a691f8beef9e06b
Jacob Carver <jacxbcarver@gmail.com>
2018-12-14 14:52:18 -0700

    added docs folder

---
+++ docs/precache-manifest.fe951610888314fc5ee660e42d2e5345.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bdf23fec5db338afcb0870d7a1d6e2a3",
+    "url": "/static/media/menu.bdf23fec.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "de00c7772d949eb52db0",
+    "url": "/static/js/main.de00c777.chunk.js"
+  },
+  {
+    "revision": "21fd930a7ffdc73264f1",
+    "url": "/static/js/1.21fd930a.chunk.js"
+  },
+  {
+    "revision": "de00c7772d949eb52db0",
+    "url": "/static/css/main.1647b320.chunk.css"
+  },
+  {
+    "revision": "a2224f7322ea55a0110d61012fa072ab",
+    "url": "/index.html"
+  }
+];
